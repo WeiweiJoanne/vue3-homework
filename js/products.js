5d82d11f5c1239954b553a746ea22f8826a43e3a
JoanneWei <joannewei0921@gmail.com>
2023-01-12 11:09:15 +0800

    feat: update product

--- js/products.js
+++ js/products.js
@@ -1,10 +1,9 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
-const url = 'https://vue3-course-api.hexschool.io/v2/'
-const path = 'petshome'
+const url = "https://vue3-course-api.hexschool.io/v2/";
+const path = "petshome";
 
 let productModal = null;
-
 
 const app = {
   data() {
@@ -13,108 +12,130 @@ const app = {
       isActive: null,
       isLoading: true,
       modalFor: null,
-      insertImgBtn: 'insert',
-      productTemp: {
-        title: '',
-        category: '',
-        unit: '',
-        origin_price: 0,
-        price: 0,
-        description: '',
-        content: '',
-        is_enabled: 0,
-        imageUrl: '',
-        imagesUrl: []
-      },
-    }
+      insertImgBtn: "insert",
+      productTemp: {},
+    };
   },
   watch: {
     productTemp: {
       handler: function (newVal) {
-        const vm = this
-        vm.insertImgBtn = newVal.imagesUrl[newVal.imagesUrl.length - 1] !== '' ? 'insert' : 'delete'
+        const vm = this;
+        if (Object.keys(vm.productTemp).includes("imagesUrl")) {
+          vm.insertImgBtn =
+            newVal.imagesUrl[newVal.imagesUrl.length - 1] !== ""
+              ? "insert"
+              : "delete";
+        }
+        vm.productTemp.is_enabled = vm.productTemp.is_enabled ? 1 : 0;
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
   mounted() {
-    this.isLogin()
-    productModal = new bootstrap.Modal(document.getElementById('productModal'), {
-      keyboard: false
-    });
-
+    this.isLogin();
+    productModal = new bootstrap.Modal(
+      document.getElementById("productModal"),
+      {
+        keyboard: false,
+      }
+    );
   },
   methods: {
-    addProduct() {
-      
-      const vm = this
-      const objVal = Object.values(vm.productTemp)
-      let isValidForm = objVal.includes('') ? false: true
-
-      if (!isValidForm){
-        alert('請將欄位填寫完畢')
-      }else{
-        const addProductAPI = `${url}api/${path}/admin/product`
-        const data = {
-          data: vm.productTemp
-        }
-        axios.post(addProductAPI, data)
-          .then(res=> {
-            vm.productTemp = { imagesUrl:[] }
-            vm.getProductAll()
-            productModal.hide();
-          })
-          .catch(err=> console.log(err))
+    openModal(todo, id) {
+      const vm = this;
+      vm.modalFor = todo;
+      if (todo == "new") {
+        vm.productTemp = {};
+      } else {
+        vm.products.forEach((el, i) => {
+          if (el.id === id) {
+            vm.productTemp = { ...vm.products[i] };
+          }
+        });
       }
-    },
-    openModal() {
+      vm.insertImgBtn = "insert";
       productModal.show();
     },
+    excute() {
+      const vm = this;
+      const objVal = Object.values(vm.productTemp);
+      const isValidForm = objVal.includes("") ? false : true;
+      const excuteAPI = vm.modalFor === "new" ? `${url}api/${path}/admin/product` : `${url}api/${path}/admin/product/${vm.productTemp.id}`
+      const method = vm.modalFor === "new" ? 'post' : `put`
+
+      if (!isValidForm) {
+        alert("請將欄位填寫完畢");
+      } else {
+        const data = {
+          data: vm.productTemp,
+        };
+        axios[method](excuteAPI, data)
+          .then((res) => {
+            vm.productTemp = {};
+            vm.getProductAll();
+            productModal.hide();
+          })
+          .catch((err) => console.log(err));
+      }
+    },
     switchImgBtn(status) {
-      const vm = this
-      vm.insertImgBtn = status
-      status === 'delete' ? vm.productTemp.imagesUrl.push('') : vm.productTemp.imagesUrl.splice(-1, 1)
+      const vm = this;
+      vm.insertImgBtn = status;
+      if (status === "delete") {
+        if (Object.keys(vm.productTemp).includes("imagesUrl")) {
+          vm.productTemp.imagesUrl.push("");
+        } else {
+          vm.productTemp.imagesUrl = [""];
+        }
+      } else {
+        vm.productTemp.imagesUrl.splice(-1, 1);
+      }
     },
     productDetail(num) {
       const vm = this;
-      vm.isActive = num
+      vm.isActive = num;
     },
-    getProductAll(){
-      const getProductsAPI = `${url}api/${path}/admin/products/all`
-      axios.get(getProductsAPI)
-        .then(res => {
-          this.products = Object.values(res.data.products)
-          this.isLoading = false
+    getProductAll() {
+      const getProductsAPI = `${url}api/${path}/admin/products/all`;
+      axios
+        .get(getProductsAPI)
+        .then((res) => {
+          this.products = Object.values(res.data.products);
+          this.isLoading = false;
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
     },
     isLogin() {
-      const vm = this
-      const token = document.cookie.replace(/(?:(?:^|.*;\s*)petsHome\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-      axios.defaults.headers.common['Authorization'] = token
+      const vm = this;
+      const token = document.cookie.replace(
+        /(?:(?:^|.*;\s*)petsHome\s*\=\s*([^;]*).*$)|^.*$/,
+        "$1"
+      );
+      axios.defaults.headers.common["Authorization"] = token;
 
-      const checkLoginAPI = `${url}api/user/check`
-      
+      const checkLoginAPI = `${url}api/user/check`;
 
-      axios.post(checkLoginAPI)
-        .then(res => {
-          vm.getProductAll()
+      axios
+        .post(checkLoginAPI)
+        .then((res) => {
+          vm.getProductAll();
         })
-        .catch(err => {
-          window.location.href = './login.html'
-        })
+        .catch((err) => {
+          window.location.href = "./login.html";
+        });
     },
     logOut() {
-      const logOutApi = `${url}logout`
-      axios.post(logOutApi)
-        .then(res => {
+      const logOutApi = `${url}logout`;
+      axios
+        .post(logOutApi)
+        .then((res) => {
           if (res.data.success) {
-            alert(res.data.message)
-            window.location.href = './login.html'
+            alert(res.data.message);
+            window.location.href = "./login.html";
           }
         })
-        .catch(err => console.log(err))
-    }
-  }
-}
-createApp(app).mount('#app')
+        .catch((err) => console.log(err));
+    },
+  },
+};
+createApp(app).mount("#app");
