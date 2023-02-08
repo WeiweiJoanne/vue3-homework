4b98896ba454c1539bc1f3c4c10d73eaba5dab85
joannewei <joannewei0921@gmail.com>
2023-02-09 01:58:35 +0800

    feat: form validate

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,33 @@
+const { Form, Field, ErrorMessage } = VeeValidate
+Object.keys(VeeValidateRules).forEach(rule => {
+  if (rule !== 'default') {
+    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+  }
+});
+VeeValidateI18n.loadLocaleFromURL('/js/zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
+
 const app = Vue.createApp({
   data(){
     return {
-      
+      user:{}
     }
-  }
+  },
+  mounted() {
+    
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit');
+    },
+  },
 })
+app.component('VForm', Form);
+app.component('VField', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.mount('#app')
