9e41f564579b33afa27ae71bcc84e386c79de85d
joannewei <joannewei0921@gmail.com>
2023-02-10 01:59:43 +0800

    feat: temProductModal,addToCart OK

--- js/cart.js
+++ js/cart.js
@@ -12,20 +12,50 @@ VeeValidate.configure({
   validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
 });
 
+import productModal from './product-modal.js';
+const url = "https://vue3-course-api.hexschool.io/v2/";
+const path = "petshome";
+
 const app = Vue.createApp({
   data(){
     return {
-      user:{}
+      user:{},
+      products:[],
+      showProductId:null
     }
   },
   mounted() {
-    
+    const vm=this
+    axios.get(`${url}api/${path}/products/all`)
+      .then(res=>{
+        vm.products = res.data.products
+      })
   },
   methods: {
     onSubmit() {
       console.log('submit');
     },
+    showProductDetail(id){
+      this.showProductId = id
+    },
+    addToCart(id,qty=1){
+     // / v2 / api / { api_path } / cart
+     const data = {
+       data: {
+         product_id: id,
+         qty: qty
+       }
+     }
+      axios.post(`${url}api/${path}/cart`,data)
+        .then(res => {
+          console.log(res);
+          this.$refs.refModal.hideModal();
+        })
+    }
   },
+  components:{
+    productModal
+  }
 })
 app.component('VForm', Form);
 app.component('VField', Field);
